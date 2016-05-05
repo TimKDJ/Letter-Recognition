5237012f1fd51717a6b81075d62606e07b29568a
TimKDJ <tim_jong@hotmail.com>
2016-05-05 14:56:38 +0200

    Initial implementation of forward propogation and weight/bias initialization

---
+++ neural_network_engine.R
@@ -0,0 +1,21 @@
+hiddenLayers <- 2
+input <- sample(0:1,25,replace=TRUE)
+inputSize <- length(input)
+
+hiddenLayer <- function() {
+  for (i in 1:hiddenLayers) {
+    
+  }
+}
+
+function someNeuron <- 
+for (i in 1:inputSize) {
+  
+}
+
+reLU <- function(x) {
+  if (x < 0) {
+    x <- 0;
+  }
+  return(x)
+}
