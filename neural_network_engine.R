30effea37a1614a65a2c671730ec7860ef787394
TimKDJ <tim_jong@hotmail.com>
2016-05-05 15:00:39 +0200

    Feedforward propogation and weight/bias initialization

--- neural_network_engine.R
+++ neural_network_engine.R
@@ -1,19 +1,53 @@
-hiddenLayers <- 2
-input <- sample(0:1,25,replace=TRUE)
-inputSize <- length(input)
+kNeuronCount <- c(25, 25, 25, 26)
+kInputCount <- c(0, 25, 25, 25)
+kLayerType <- c('input', 'hidden', 'hidden', 'output')
+kLayerCount <- 4
+kBiasBound <- 0.5
+kWeightBound <- 0.5
+kLearningRate <- 0.01
 
-hiddenLayer <- function() {
-  for (i in 1:hiddenLayers) {
-    
+B <- vector('list', kLayerCount)
+W <- vector('list', kLayerCount)
+input <- sample(0:1,25,replace=TRUE)
+
+TrainNeuralNetwork <- function() {
+  InitWeightsBiases()
+  output <- ForwardPropogation()
+  print(output)
+  GradientDescent()
+}
+
+#init weights and biases for hidden layers and the output layer (+1)
+InitWeightsBiases <- function() {
+  for (i in 2:kLayerCount) {
+    B[[i]] <<- runif(kInputCount[i], -kBiasBound, kBiasBound)
+    for (j in 1:kInputCount[i]) {
+      W[[i]][[j]] <<- runif(kInputCount[i], -kWeightBound, kWeightBound)
+    }
   }
 }
 
-function someNeuron <- 
-for (i in 1:inputSize) {
-  
+#feedforward through network from input to output
+ForwardPropogation <- function() {
+  previousLayerOutput <- input
+  tmp <- numeric(0);
+  for (i in 2:kLayerCount) {
+    for (j in 1:kInputCount[i]) {
+      tmp[j] <- CalculateNeuronOutput(W[[i]][[j]], B[[i]][[j]], previousLayerOutput)
+    }
+    previousLayerOutput <- tmp
+    tmp <- numeric(0)
+  }
+  return(previousLayerOutput)
 }
 
-reLU <- function(x) {
+CalculateNeuronOutput <- function(weight, bias, inputs) {
+  sum <- sum(weight * inputs + bias)
+  output <- reLU(sum)
+  return(output)
+}
+
+reLU <- function(x, deriv = FALSE) {
   if (x < 0) {
     x <- 0;
   }
