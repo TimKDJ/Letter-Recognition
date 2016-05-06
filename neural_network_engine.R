85f622ee407799d94fd34df64a68e48c189508f2
TimKDJ <tim_jong@hotmail.com>
2016-05-06 10:35:04 +0200

    Improved forward  propogation, added softmax function

--- neural_network_engine.R
+++ neural_network_engine.R
@@ -2,13 +2,14 @@ kNeuronCount <- c(25, 25, 25, 26)
 kInputCount <- c(0, 25, 25, 25)
 kLayerType <- c('input', 'hidden', 'hidden', 'output')
 kLayerCount <- 4
-kBiasBound <- 0.5
-kWeightBound <- 0.5
+kBiasBound <- 0.1
+kWeightBound <- 0.1
 kLearningRate <- 0.01
 
 B <- vector('list', kLayerCount)
 W <- vector('list', kLayerCount)
-input <- sample(0:1,25,replace=TRUE)
+input <- c(0,0,0,0,0,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,0,0,0,0,0)
+
 
 TrainNeuralNetwork <- function() {
   InitWeightsBiases()
@@ -17,39 +18,49 @@ TrainNeuralNetwork <- function() {
   GradientDescent()
 }
 
-#init weights and biases for hidden layers and the output layer (+1)
+#init weights and biases for hidden layers and the output layer
 InitWeightsBiases <- function() {
   for (i in 2:kLayerCount) {
-    B[[i]] <<- runif(kInputCount[i], -kBiasBound, kBiasBound)
-    for (j in 1:kInputCount[i]) {
-      W[[i]][[j]] <<- runif(kInputCount[i], -kWeightBound, kWeightBound)
-    }
+    B[[i]] <<- runif(kNeuronCount[i], -kBiasBound, kBiasBound)
+    W[[i]] <<- matrix(runif(kInputCount[i] * kNeuronCount[i], -kWeightBound, kWeightBound), kInputCount[i], kNeuronCount[i])
   }
 }
 
 #feedforward through network from input to output
 ForwardPropogation <- function() {
   previousLayerOutput <- input
-  tmp <- numeric(0);
   for (i in 2:kLayerCount) {
-    for (j in 1:kInputCount[i]) {
-      tmp[j] <- CalculateNeuronOutput(W[[i]][[j]], B[[i]][[j]], previousLayerOutput)
-    }
-    previousLayerOutput <- tmp
-    tmp <- numeric(0)
+    previousLayerOutput <- CalculateLayerOutput(W[[i]], B[[i]], previousLayerOutput, kLayerType[i])
   }
   return(previousLayerOutput)
 }
 
-CalculateNeuronOutput <- function(weight, bias, inputs) {
-  sum <- sum(weight * inputs + bias)
-  output <- reLU(sum)
+CalculateLayerOutput <- function(weights, bias, inputs, layer) {
+  totalInput <- colSums(inputs * weights) + bias
+  if (layer == 'hidden') {
+    output <- ActivationFunction(totalInput)
+  } else {
+    output <- SoftmaxFunction(totalInput)
+  }
   return(output)
 }
 
-reLU <- function(x, deriv = FALSE) {
-  if (x < 0) {
-    x <- 0;
-  }
-  return(x)
+ActivationFunction <- function(x, deriv = FALSE) {
+  result <- 1 / (1 + exp(-x))
+  return(result)
+}
+
+SoftmaxFunction <- function(x) {
+  result <- exp(x) / sum(exp(x))
+  return(result)
+}
+
+GetCorrectOutput <- function(letter) {
+  output <- as.numeric(letters == letter)
+  return(output)
+}
+
+
+BackwardPropogation <- function() {
+  
 }
